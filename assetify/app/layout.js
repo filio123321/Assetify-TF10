eda28b417e8a65150aa1906105c8b88b71e2ec37
Filostratos <ftitopoulos@gmail.com>
2024-03-20 11:00:38 +0200

    navbar and next installation

--- assetify/app/layout.js
+++ assetify/app/layout.js
@@ -1,5 +1,8 @@
+import { Providers } from "@/providers";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
+import NavigationBar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +13,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <Providers>
+          <NavigationBar />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
