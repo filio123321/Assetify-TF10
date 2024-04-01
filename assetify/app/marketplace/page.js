04c7fdc2024cb1a14524908a8c77888822b75afa
Filostratos <ftitopoulos@gmail.com>
2024-04-01 22:28:34 +0300

    marketplace text title

--- assetify/app/marketplace/page.js
+++ assetify/app/marketplace/page.js
@@ -26,27 +26,16 @@ export default function Marketplace() {
 
     return (
         <main className="flex min-h-screen w-full flex-wrap justify-center p-4 gap-4"> {/* Grid layout */}
-            {isPending && <div>Loading assets...</div>}
-            {assets.map((asset, index) => (
-                // <Card key={index} className="w-56 flex flex-col items-stretch shadow-lg rounded-lg overflow-hidden"> {/* Adjusted for consistent width */}
-                //     <CardHeader className=" pt-2 px-4 flex-col items-start">
 
-                //         <h4 className="font-bold text-lg">{asset.name}</h4>
-                //         <p className="text-sm uppercase font-bold">{asset.pricePerShare} ETH</p>
-                //         {/* <small className="">{asset.owner}</small>
-                //         <h1 className="text-sm">{JSON.stringify(asset.ipfsHashes)}</h1> */}
-                //     </CardHeader>
-                //     <CardBody className="overflow-visible py-2">
-                //             <div className="w-full h-auto">
-                //                 <Image
-                //                     alt="Asset image"
-                //                     className="w-full h-auto object-cover rounded-xl" // Adjusted for responsive images
-                //                     src='https://ipfs.io/ipfs/QmWbYTUVV8GmB2EW8qn5CF3eF9aLW3msjr2cS2izckKadu/406679791_122102880962132522_5789972342587448293_n.jpg'
-                //                 />
-                //             </div>
-                //         </CardBody>
-                // </Card>
-                <Card key={index} className="py-4">
+            {/* TItle */}
+            <div className="w-full flex items-center justify-center mb-16">
+                <div className='font-bold lg:text-5xl text-4xl text-center max-w-screen-auto drop-shadow-lg pt-8 max-w-[60%]'>
+                    <p className="bg-gradient-to-r from-pink-500 to-indigo-500 inline-block text-transparent bg-clip-text">Marketplace</p>
+                </div>
+            </div>
+
+            {assets.map((asset, index) => (
+                <Card key={index} className="py-4 h-full" isPressable>
                     <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                         <p className="text-tiny uppercase font-bold">Available {asset.sharesAvailable}/{asset.totalShares}</p>
                         <small className="text-default-500">{asset.pricePerShare} ETH</small>
