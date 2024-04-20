b45a85cb361e8549424ed269a4da6c575f9a4983
filio <ftitopoulos@gmail.com>
2024-04-21 00:35:43 +0300

    image not rendered solution

--- assetify/components/marketListing.jsx
+++ assetify/components/marketListing.jsx
@@ -2,21 +2,18 @@ import React, { useState, useEffect, useContext } from 'react';
 import { AppContext } from "@/context/context";
 import ListingBuy from '@/components/listingBuy';
 import ListingSell from '@/components/listingSell';
-import { Card, CardHeader, CardBody, CardFooter, Button, Tooltip, Skeleton, Image } from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter, Button, Tooltip, Skeleton } from "@nextui-org/react";
 
 function MarketListing(props) {
     const { asset } = props;
-    const [imageLoaded, setImageLoaded] = useState(false);
-    const [imageSrc, setImageSrc] = useState(`https://923c0163885cbdec43fe9f8f82870f09.ipfscdn.io/ipfs/${asset.ipfsHashes[0] && asset.ipfsHashes[0].split('ipfs://')[1]}`);
+    const [contentLoaded, setContentLoaded] = useState(false);
+    const [contentType, setContentType] = useState('image');
+    const [contentSrc, setContentSrc] = useState('');
     const { account, checkUserShareOwnership } = useContext(AppContext);
     const [ownsShares, setOwnsShares] = useState(false);
     const [isOwner, setIsOwner] = useState(false);
     const [openBuyMenu, setOpenBuyMenu] = useState(false);
     const [openSellMenu, setOpenSellMenu] = useState(false);
-
-    const handleImageLoad = () => {
-        setImageLoaded(true);
-    };
 
     useEffect(() => {
         const checkOwnership = async () => {
@@ -29,14 +26,46 @@ function MarketListing(props) {
             checkOwnership();
         }
 
-        const imageLoadTimeout = setTimeout(() => {
-            if (!imageLoaded) {
-                setImageSrc('/not_rendered.png');
+        const url = `https://923c0163885cbdec43fe9f8f82870f09.ipfscdn.io/ipfs/${asset.ipfsHashes[0] && asset.ipfsHashes[0].split('ipfs://')[1]}`;
+        fetch(url, { method: 'HEAD' })
+            .then(response => {
+                const fileType = response.headers.get('Content-Type');
+                if (fileType.startsWith('image')) {
+                    setContentType('image');
+                    setContentSrc(url);
+                } else if (fileType.startsWith('video')) {
+                    setContentType('video');
+                    setContentSrc(url);
+                } else if (fileType === 'application/pdf') {
+                    setContentType('pdf');
+                    setContentSrc(url);
+                } else {
+                    setContentSrc('/not_rendered.png');
+                }
+            })
+            .catch(() => setContentSrc('/not_rendered.png'));
+
+        const loadTimeout = setTimeout(() => {
+            if (!contentLoaded) {
+                setContentSrc('/not_rendered.png');
             }
         }, 5000);
 
-        return () => clearTimeout(imageLoadTimeout);
-    }, [account, asset, checkUserShareOwnership, imageLoaded]);
+        return () => clearTimeout(loadTimeout);
+    }, [account, asset, checkUserShareOwnership]);
+
+    const renderContent = () => {
+        switch (contentType) {
+            case 'image':
+                return <img src={contentSrc} alt="Card background" className="object-cover rounded-xl" onLoad={() => setContentLoaded(true)} />;
+            case 'video':
+                return <video src={contentSrc} controls className="w-full h-auto rounded-xl" onLoadedData={() => setContentLoaded(true)} />;
+            case 'pdf':
+                return <iframe src={contentSrc} className="w-full h-auto rounded-xl" onLoad={() => setContentLoaded(true)} />;
+            default:
+                return <p>Unsupported file type</p>;
+        }
+    };
 
     return (
         <>
@@ -47,15 +76,9 @@ function MarketListing(props) {
                     <h4 className="font-bold text-large">{asset.name}</h4>
                 </CardHeader>
                 <CardBody className="overflow-visible py-2">
-                    {!imageLoaded && <Skeleton className="aspect-square w-auto rounded-lg" />}
+                    {!contentLoaded && <Skeleton className="aspect-square w-auto rounded-lg" />}
                     <div className="p-1 flex items-center">
-                        <Image
-                            alt="Card background"
-                            className={`object-cover rounded-xl ${!imageLoaded ? 'hidden' : ''}`}
-                            src={imageSrc}
-                            width={270}
-                            onLoad={handleImageLoad}
-                        />
+                        {renderContent()}
                     </div>
                 </CardBody>
                 <CardFooter>
